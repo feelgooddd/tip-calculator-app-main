ad1fb2e63e882e85dd7a682804dadbab55792c2c
Feelgooddd <fortuneryanc@gmail.com>
2025-01-17 00:10:55 -0500

    Fixed isues with the text for tip amount / person and total amount / person splitting up incorrectly when their values were different lengths. Added comments to code. Fixed the reset button to set everything to be empty instead of filling them with the value of 0.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//variables to reference several elements on the page
 const tipButton = document.querySelectorAll(".tip-button");
 const inputCustom = document.getElementById("custom-tip");
 const inputBillAmount = document.getElementById("bill-amount");
@@ -8,11 +9,14 @@ const invalidPeople = document.getElementById("invalid-number-of-people");
 let totalAmount = document.getElementById("total-amount");
 let tipAmount = document.getElementById("tip-amount");
 
+//event listeners for input text boxes
 inputCustom.addEventListener("input", tipCustom);
 inputBillAmount.addEventListener("input", tipCustom);
 inputNumberOFPeople.addEventListener("input", tipCustom);
 
+//Get CSS root variables for colours
 const root = document.documentElement;
+
 const primary400 = getComputedStyle(root).getPropertyValue("--clr-primary-400");
 const accent400 = getComputedStyle(root).getPropertyValue("--clr-accent-400");
 const neutral600 = getComputedStyle(root).getPropertyValue("--clr-neutral-600");
@@ -23,8 +27,10 @@ const neutral200 = getComputedStyle(root).getPropertyValue("--clr-neutral-200");
 const neutral100 = getComputedStyle(root).getPropertyValue("--clr-neutral-100");
 5;
 
+//slider element at bottom of page
 const colourCheckbox = document.getElementById("colourCheckBox");
 
+// functions for calculating tip & Total
 function calculateTip(tip, numberOfPeople) {
   let billInput = Number(document.getElementById("bill-amount").value);
   let tipAmount = document.getElementById("tip-amount");
@@ -36,7 +42,6 @@ function calculateTip(tip, numberOfPeople) {
   numberOfPeople = parseFloat(numberOfPeople);
 
   handleUserInputError(tipAmount);
-  handleBillInputError();
 
   if (isNaN(billInput)) {
     invalidBill.classList.remove("hidden");
@@ -53,6 +58,8 @@ function calculateTotal(tipAmount, numberOfPeople) {
   handleUserInputError(totalAmount);
   //debugging consolelogs
 }
+
+// Functions for tip buttons and custom tip input box
 function tipFive() {
   let numberOfPeople = document.getElementById("number-of-people").value;
   calculateTip(0.05, numberOfPeople);
@@ -86,14 +93,17 @@ function tipCustom() {
   calculateTotal(1 + customTipAmount, numberOfPeople);
 }
 
+//function to reset the page when the reset button is clicked, called in the HTML by the buttons onclick property.
 function resetNumbers() {
-  inputCustom.value = "0";
-  inputBillAmount.value = "0";
-  inputNumberOFPeople.value = "0";
+  inputCustom.value = "";
+  inputBillAmount.value = "";
+  inputNumberOFPeople.value = "";
   totalAmount.textContent = "$0.00";
   tipAmount.textContent = "$0.00";
-  console.log("meow");
 }
+
+//helper class to check if the user input resolves to $NaN or $Infinity then setting it to 0 instead.
+// (Infinity in the case of dividing by 0 such as the number of people being 0)
 function handleUserInputError(tipAmount) {
   if (
     tipAmount.textContent === "$NaN" ||
@@ -103,12 +113,10 @@ function handleUserInputError(tipAmount) {
   ) {
     tipAmount.textContent = "$0.00";
     totalAmount.textContent = "$0.00";
-    console.log("meow");
   }
 }
 
-function handleBillInputError(billAmount) {}
-
+//Functions to change the CSS :root elment variables for the colours on the page.
 function changeColourScheme() {
   root.style.setProperty("--clr-primary-400", "hsl(199, 100.00%, 18.00%)");
   root.style.setProperty("--clr-accent-400", "hsl(248, 27.60%, 43.30%");
@@ -131,6 +139,7 @@ function revertColourScheme() {
   root.style.setProperty("--clr-neutral-100", neutral100);
 }
 
+//event listener on the checkbox to call the colour changing functions when the checkbox is checked or unchecked.
 colourCheckbox.addEventListener("change", function () {
   if (this.checked) {
     changeColourScheme();
